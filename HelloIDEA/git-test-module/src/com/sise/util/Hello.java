46d7a6233d67657ad456e83a04191eaca15679f6
ClearLove艳 <972640834@qq.com>
2020-07-25 17:35:40 +0800

    把本地工程添加到git

---
+++ HelloIDEA/git-test-module/src/com/sise/util/Hello.java
@@ -0,0 +1,11 @@
+package com.sise.util;
+
+/**
+ * @author 645
+ * @date 2020-07-25 17:24
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello Git!");
+    }
+}
