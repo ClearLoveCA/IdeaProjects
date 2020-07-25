c300f201bd14ae17e73aece38c3953b86add2c0d
ClearLove艳 <972640834@qq.com>
2020-07-25 17:58:41 +0800

    又更新git-test

--- HelloIDEA/git-test-module/src/com/sise/util/Hello.java
+++ HelloIDEA/git-test-module/src/com/sise/util/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
     public void test(){
         System.out.println("Hello GGGit!");
